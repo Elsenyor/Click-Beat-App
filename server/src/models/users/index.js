83b53059e012f5bc0295e138f53b59a9395d983d
Tomás Guijarro Ferrandiz <tomasguijarroferrandiz@gmail.com>
2024-10-09 12:06:33 +0200

    recover password flux

--- server/src/models/users/index.js
+++ server/src/models/users/index.js
@@ -2,5 +2,6 @@ import insertUserModel from "./insertUserModel.js";
 import validateUserModel from "./validateUserModel.js";
 import selectUserByEmailModel from "./selectUserByEmailModel.js";
 import selectUserByIdModel from "./selectUserByIdModel.js";
+import recoverPasswordModel from "./recoverPasswordModel.js";
 
-export { insertUserModel, validateUserModel, selectUserByEmailModel, selectUserByIdModel };
+export { insertUserModel, validateUserModel, selectUserByEmailModel, selectUserByIdModel, recoverPasswordModel };
